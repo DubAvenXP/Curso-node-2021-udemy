d5d80e28bcae053323a3f01315f0c2261cf28048
Alejandro Dubon <alejandrodubon88@gmail.com>
2021-06-16 16:23:59 -0600

    primeros cambios

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('../database/config');
+const fileUpload = require('express-fileupload');
 class Server {
 
 
@@ -13,6 +14,7 @@ class Server {
             buscar: '/api/buscar',
             categorias: '/api/categorias',
             productos: '/api/productos',
+            uploads: '/api/uploads',
             usuarios: '/api/usuarios',
         }
 
@@ -28,11 +30,17 @@ class Server {
     async conectarDB(){
         await dbConnection();
     }
-
+    //Middlewares a nivel de servidor
     middlewares(){
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.static('public'));
+        
+        // Fileupload - carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
     }
 
     routes() {
@@ -40,6 +48,7 @@ class Server {
         this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
